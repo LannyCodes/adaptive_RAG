a9180e9f03f46df62c1c0e76a4a1a34283086796
lanny xu <pro@192.168.10.22>
2025-12-02 11:13:32 +0800

    add Milvus db

--- document_processor.py
+++ document_processor.py
@@ -4,9 +4,9 @@
 """
 
 try:
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-except ImportError:
     from langchain_text_splitters import RecursiveCharacterTextSplitter
+except ImportError:
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
