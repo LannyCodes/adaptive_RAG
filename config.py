633028f57ff1fe1372ce0ce8ad7e1bb8cfc62bfd
LannyCodes <709282865@qq.com>
2026-04-23 00:51:23 +0800

    添加新的ULR

--- config.py
+++ config.py
@@ -55,6 +55,7 @@ KNOWLEDGE_BASE_URLS = [
     "https://lilianweng.github.io/posts/2023-06-23-agent/",
     "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
     "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+    "https://learnprompting.org/docs/introduction"
 ]
 
 # 文档分块配置
